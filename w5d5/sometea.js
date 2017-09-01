a0062c191bf0c160c7faa716883542177d96d90f
lisza <liszalisza@gmail.com>
2017-09-01 13:44:07 -0700

    fix readline interface typo

--- w5d5/sometea.js
+++ w5d5/sometea.js
@@ -2,13 +2,13 @@ const readline = require('readline');
 
 const reader = readline.createInterface({
   input: process.stdin,
-  output: process.stout
+  output: process.stdout
 });
 
 function teaAndBiscuits(){
   reader.question("Would you like some tea?", function(response1){
       console.log(`Your response was ${response1}.`);
-      reader.question("Would you like some biscuits?", function (response2) {
+      reader.question("Would you like some biscuits?", function (response2){
         console.log(`Your response was ${response2}.`);
 
       const tea = (response1 === 'yes') ? 'do' : 'don\'t';
@@ -19,3 +19,5 @@ function teaAndBiscuits(){
     });
   });
 }
+
+teaAndBiscuits();
